3d7916dbd41b034018f13c2ae6df9768f50be3db
NakagamiYuta <guguru0014@gmail.com>
2023-04-27 17:51:08 +0900

    [add] tests/に"impl_orig_target"を追加

---
+++ tests/impl_orig_target/impl_orig_target.rs
@@ -0,0 +1,24 @@
+use depagerpp::builder::prelude::*;
+use depagerpp::builder::Builder;
+
+struct OrigEnvSettings;
+
+impl Environment for OrigEnvSettings {
+    fn name() -> &'static str { "Original" }
+}
+
+#[derive(Default)]
+struct OrigTarget;
+
+impl Target<OrigEnvSettings> for OrigTarget {
+    fn build(&self) {
+        // do nothing
+    }
+}
+
+#[test]
+fn check_orig_target() {
+    Builder::new()
+        .add(OrigTarget::default())
+        .build()
+}
