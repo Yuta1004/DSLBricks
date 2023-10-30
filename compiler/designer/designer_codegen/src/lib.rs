d2cb1cb0e63712bad3ab0c3cfb5f919511e48ec2
NakagamiYuta <guguru0014@gmail.com>
2023-10-31 07:06:52 +0900

    [add] designer_codegen 仮実装

---
+++ compiler/designer/designer_codegen/src/lib.rs
@@ -0,0 +1,36 @@
+use serde::Serialize;
+use tinytemplate::{format_unescaped, TinyTemplate};
+
+use design::DSLDesign;
+
+#[allow(non_snake_case)]
+#[derive(Serialize)]
+struct CodeTemplate {
+    TOKEN_DEFS: String,
+    SYNTAX_DEFS: String,
+    BNF: String,
+}
+
+pub fn gen(_design: Box<dyn DSLDesign>) -> anyhow::Result<String> {
+    let token_defs = gen_token_code()?;
+    let (syntax_defs, bnf) = gen_syntax_code()?;
+    let context = CodeTemplate {
+        TOKEN_DEFS: token_defs,
+        SYNTAX_DEFS: syntax_defs,
+        BNF: bnf,
+    };
+
+    let mut tt = TinyTemplate::new();
+    tt.set_default_formatter(&format_unescaped);
+    tt.add_template("Template", include_str!("../template/template.txt"))?;
+
+    Ok(tt.render("Template", &context)?)
+}
+
+fn gen_token_code() -> anyhow::Result<String> {
+    Ok("".to_string())
+}
+
+fn gen_syntax_code() -> anyhow::Result<(String, String)> {  // (syntax_defs, bnf)
+    Ok(("".to_string(), "".to_string()))
+}
