3cff91dd37e525dd452ede1b395022dd1e77a0ad
NakagamiYuta <guguru0014@gmail.com>
2023-10-31 06:15:04 +0900

    [add] DSLDesign, DSLPart, SyntaxElem 構造体定義

---
+++ compiler/designer/designer_design/src/lib.rs
@@ -0,0 +1,60 @@
+pub trait DSLDesign {
+    fn design(&self) -> Vec<Box<dyn DSLPart>>;
+}
+
+pub trait DSLPart {
+    fn syntax(&self) -> Vec<SyntaxElem>;
+
+    fn tokens(&self) -> Vec<&'static str> {
+        self.syntax()
+            .into_iter()
+            .map(|elem| {
+                match elem {
+                    SyntaxElem::Const(s) => vec![s],
+                }
+            })
+            .flatten()
+            .collect()
+    }
+}
+
+pub enum SyntaxElem {
+    Const(&'static str),
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{DSLDesign, DSLPart, SyntaxElem};
+
+    struct MyDSL;
+
+    impl DSLDesign for MyDSL {
+        fn design(&self) -> Vec<Box<dyn DSLPart>> {
+            vec![Box::new(Function)]
+        }
+    }
+
+    struct Function;
+
+    impl DSLPart for Function {
+        fn syntax(&self) -> Vec<SyntaxElem> {
+            vec![
+                SyntaxElem::Const("func"),
+                SyntaxElem::Const("("),
+                SyntaxElem::Const(")"),
+                SyntaxElem::Const("{"),
+                SyntaxElem::Const("}"),
+            ]
+        }
+    }
+
+    #[test]
+    fn tokens() {
+        let tokens: Vec<&str> = MyDSL.design()
+            .into_iter()
+            .map(|part| part.tokens())
+            .flatten()
+            .collect();
+        assert_eq!(tokens, vec!["func", "(", ")", "{", "}"])
+    }
+}
