47d8cb79664b9f82a059b26e8d7e1cbf1e7ecd65
NakagamiYuta <guguru0014@gmail.com>
2023-11-15 03:10:24 +0900

    [remove] tests/

--- tests/executor/compiler.rs
+++
@@ -1,8 +0,0 @@
-use blockdsl_compiler::executor::Compiler;
-use blockdsl_lib::dev::dsl::*;
-
-#[test]
-fn compiler() {
-    let dsl = expr_dsl();
-    let _ = Compiler::<ExprNode, ExprSyntax, ExprToken>::from(dsl);
-}
--- tests/executor/interpreter.rs
+++
@@ -1,8 +0,0 @@
-use blockdsl_compiler::executor::Interpreter;
-use blockdsl_lib::dev::dsl::*;
-
-#[test]
-fn interpreter() {
-    let dsl = expr_dsl();
-    let _ = Interpreter::<ExprNode, ExprSyntax, ExprToken>::from(dsl);
-}
