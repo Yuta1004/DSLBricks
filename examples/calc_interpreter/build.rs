d8c7b4b76e054496e0f44679981ac692f2ef7a5a
NakagamiYuta <guguru0014@gmail.com>
2023-11-17 04:37:48 +0900

    [add] designer_design::rule! マクロ作成

--- examples/calc_interpreter/build.rs
+++ examples/calc_interpreter/build.rs
@@ -1,6 +1,6 @@
 use compiler::build_dsl;
 use compiler::designer::design::syntax::{Rule, RuleSet, SyntaxElem};
-use compiler::designer::design::DSLGeneratable;
+use compiler::designer::design::{rule, DSLGeneratable};
 
 #[derive(Debug)]
 struct Expression;
@@ -16,14 +16,14 @@ impl DSLGeneratable for Expression {
 
     fn design(&self) -> RuleSet {
         vec![
-            Rule::from(("expr", vec![SyntaxElem::NonTerm("expr"), SyntaxElem::Term(r"\+"), SyntaxElem::NonTerm("term")])),
-            Rule::from(("expr", vec![SyntaxElem::NonTerm("expr"), SyntaxElem::Term(r"-"), SyntaxElem::NonTerm("term")])),
-            Rule::from(("expr", vec![SyntaxElem::NonTerm("term")])),
-            Rule::from(("term", vec![SyntaxElem::NonTerm("term"), SyntaxElem::Term(r"\*"), SyntaxElem::NonTerm("fact")])),
-            Rule::from(("term", vec![SyntaxElem::NonTerm("term"), SyntaxElem::Term(r"/"), SyntaxElem::NonTerm("fact")])),
-            Rule::from(("term", vec![SyntaxElem::NonTerm("fact")])),
-            Rule::from(("fact", vec![SyntaxElem::Term(r"\("), SyntaxElem::NonTerm("expr"), SyntaxElem::Term(r"\)")])),
-            Rule::from(("fact", vec![SyntaxElem::Hole(Box::new(CalcUnit))])),
+            rule! { expr -> expr r"\+" term },
+            rule! { expr -> expr r"-" term },
+            rule! { expr -> term },
+            rule! { term -> term r"\*" fact },
+            rule! { term -> term r"/" fact },
+            rule! { term -> fact },
+            rule! { fact -> r"\(" expr r"\)" },
+            rule! { fact -> [CalcUnit] },
         ]
         .into()
     }
@@ -43,8 +43,8 @@ impl DSLGeneratable for CalcUnit {
 
     fn design(&self) -> RuleSet {
         vec![
-            Rule::from(("unit", vec![SyntaxElem::Term(r"(0|[1-9][0-9]*)")])),
-            Rule::from(("unit", vec![SyntaxElem::Term(r"[a-z](_|[a-z0-9])*")])),
+            rule! { unit -> r"(0|[1-9][0-9]*)" },
+            rule! { unit -> r"[a-z](_|[a-z0-9])*" }
         ]
         .into()
     }
