118f4b2379eb8008b1ff3ed580ecec86286b91ef
NakagamiYuta <guguru0014@gmail.com>
2023-11-18 00:42:41 +0900

    [update] processor_lexer_macros 構造変更 & lexer に関するマクロを移動

--- compiler/processor/processor/src/lib.rs
+++ compiler/processor/processor/src/lib.rs
@@ -1,11 +1,11 @@
 pub use dslgen::*;
-pub use lexer;
+pub mod lexer {
+    pub use lexer::*;
+    pub use lexer_macros as macros;
+}
 pub mod parser {
     pub use parser::*;
     pub use parser_macros as macros;
-}
-pub mod macros {
-    pub use lexer_macros::*;
 }
 pub mod prelude {
     #[cfg(feature = "with-serde")]
