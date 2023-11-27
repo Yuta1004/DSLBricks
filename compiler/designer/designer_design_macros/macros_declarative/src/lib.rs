38318ba074d438ba240d26bd70fb52facdd1f545
NakagamiYuta <guguru0014@gmail.com>
2023-11-28 03:33:21 +0900

    [update] design_macros

--- compiler/designer/designer_design_macros/macros_declarative/src/lib.rs
+++ compiler/designer/designer_design_macros/macros_declarative/src/lib.rs
@@ -12,7 +12,7 @@ macro_rules! rule {
     };
 
     (@call [$design:ident]) => {
-        SyntaxElem::Hole(Rc::new(Box::new($design)))
+        SyntaxElem::Hole(Rc::new($design))
     };
 
     (@call [{$design:expr}]) => {
