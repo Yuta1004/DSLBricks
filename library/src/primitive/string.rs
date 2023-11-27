7871ec84d23d4c3be12a68eed647af03d4d52643
NakagamiYuta <guguru0014@gmail.com>
2023-11-28 04:25:40 +0900

    [change] primitive.rs -> primitive/{number, string, symbol, boolean}.rs

---
+++ library/src/primitive/string.rs
@@ -0,0 +1,41 @@
+use std::rc::Rc;
+
+use compiler::designer::constraint::ctime::impl_constraints;
+use compiler::designer::design::macros::*;
+use compiler::designer::design::syntax::RuleSet;
+use compiler::designer::design::DSLGeneratable;
+
+use crate::common::DSLBlock;
+use crate::constraints::ctime::*;
+
+/// # 文字列
+///
+/// ## 概要
+///
+/// - 「"」で囲まれた任意の文字列を表現します
+///
+/// ## 性質
+///
+/// - StaticValue
+#[impl_constraints(StaticValue)]
+pub struct String;
+
+impl DSLBlock for String {
+    fn new() -> Rc<Self> {
+        Rc::new(String)
+    }
+}
+
+impl DSLGeneratable for String {
+    fn name(&self) -> &'static str {
+        "std.primitive.String"
+    }
+
+    fn start(&self) -> &'static str {
+        "string"
+    }
+
+    fn design(&self) -> RuleSet {
+        vec![rule! { string -> r#"".*""# }].into()
+    }
+}
