8e865259291ecbc0cd8245661a2ced4129f90393
NakagamiYuta <guguru0014@gmail.com>
2023-12-15 14:42:08 +0900

    [add] Editor 下部に状態表示を追加

--- src/editor/tauri/src-js/src/components/Editor.tsx
+++ src/editor/tauri/src-js/src/components/Editor.tsx
@@ -14,7 +14,8 @@ type EditorProps = {
 export default function Editor(props: EditorProps) {
     var ws: WorkspaceSvg = null;
 
-    const [xml, setXml] = useState<string>("");
+    const [xml, setXml] = useState<string>("<xml xmlns='https://developers.google.com/blockly/xml'/>");
+    const [status, setStatus] = useState<string>("Initialized");
 
     useEffect(() => {
         if (ws) {
@@ -23,15 +24,27 @@ export default function Editor(props: EditorProps) {
     }, [props.noticeResize]);
 
     return (
-        <BlocklyWorkspace
-            className="fullscreen"
-            toolboxConfiguration={ToolBox}
-            initialXml={xml}
-            onXmlChange={(xml) => {
-                setXml(xml);
-                props.onUpdate(xml);
-            }}
-            onWorkspaceChange={(newWs) => { ws = newWs; }}
-        />
+        <div className="workspace-outer">
+            <BlocklyWorkspace
+                className="workspace"
+                toolboxConfiguration={ToolBox}
+                initialXml={xml}
+                onXmlChange={(xml) => {
+                    setXml(xml);
+                    props.onUpdate(xml);
+                }}
+                onWorkspaceChange={(newWs) => { ws = newWs; }}
+            />
+            <p
+                style={{
+                    width: "100%",
+                    height: "14px",
+                    margin: "0px",
+                    fontSize: "14px",
+                }}
+            >
+                {status}
+            </p>
+        </div>
     );
 }
