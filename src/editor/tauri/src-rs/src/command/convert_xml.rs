59fde86c2163100fb17bd67096f8095d3db092de
NakagamiYuta <guguru0014@gmail.com>
2023-12-17 02:12:56 +0900

    [add] use ...; コードを生成するようにした

--- src/editor/tauri/src-rs/src/command/convert_xml.rs
+++ src/editor/tauri/src-rs/src/command/convert_xml.rs
@@ -14,12 +14,23 @@ pub fn convert_xml(xml: &str) -> Result<String, InvokeError> {
 }
 
 struct DSLBuildFunc {
+    uses: Vec<String>,
     declares: Vec<BrickDeclare>,
 }
 
 impl From<Vec<BlocklyIR>> for DSLBuildFunc {
     fn from(irs: Vec<BlocklyIR>) -> Self {
+        let uses = irs
+            .iter()
+            .map(|ir| {
+                let mut ty = ir.ty.split(".");
+                let _ = ty.next();
+                format!("use catalog::{};", ty.collect::<Vec<&str>>().join("::"))
+            })
+            .collect::<Vec<String>>();
+
         DSLBuildFunc {
+            uses,
             declares: irs.into_iter().flat_map(BrickDeclare::from).collect(),
         }
     }
@@ -27,12 +38,13 @@ impl From<Vec<BlocklyIR>> for DSLBuildFunc {
 
 impl Display for DSLBuildFunc {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let uses = self.uses.join("\n");
         let declares = self.declares
             .iter()
             .map(|declare| format!("{}", declare))
             .collect::<Vec<String>>()
             .join("\n\n");
-        write!(f, "#[combine_bricks]\nfn main() {{\n{}\n}}", declares)
+        write!(f, "{}\n\n#[combine_bricks]\nfn main() {{\n{}\n}}", uses, declares)
     }
 }
 
@@ -60,7 +72,7 @@ impl BrickDeclare {
         }
 
         if let Some(var) = var {
-            let ty = ir.ty;
+            let ty = ir.ty.split('.').last().unwrap().to_string();
             Some(BrickDeclare { ty, var, fields })
         } else {
             None
@@ -85,24 +97,24 @@ impl Display for BrickDeclare {
 
 struct BrickDeclareField {
     name: String,
-    uses: Vec<String>,
+    variables: Vec<String>,
 }
 
 impl From<Vec<BlocklyIR>> for BrickDeclareField {
     fn from(irs: Vec<BlocklyIR>) -> Self {
         BrickDeclareField {
             name: "dummy".to_string(),
-            uses: vec![],
+            variables: vec![],
         }
     }
 }
 
 impl Display for BrickDeclareField {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        if self.uses.len() != 1 {
-            write!(f, "        {}: [{}],", self.name, self.uses.join(","))
+        if self.variables.len() != 1 {
+            write!(f, "        {}: [{}],", self.name, self.variables.join(","))
         } else {
-            write!(f, "        {}: {},", self.name, self.uses[0])
+            write!(f, "        {}: {},", self.name, self.variables[0])
         }
     }
 }
