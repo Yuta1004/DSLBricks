dab0def0415bf01772580da4aa2f31f4fd944359
NakagamiYuta <guguru0014@gmail.com>
2023-12-18 18:15:57 +0900

    [add] とりあえず rocket を使って web-server が立ち上がるようにした

--- docs/web/src/main.rs
+++ docs/web/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("hello, docs_web");
+use rocket::config::{Config, Environment};
+use rocket_contrib::serve::StaticFiles;
+
+fn main() -> anyhow::Result<()> {
+    let config = Config::build(Environment::Staging)
+        .address("0.0.0.0")
+        .port(5555)
+        .finalize()?;
+
+    rocket::custom(config)
+        .mount("/", StaticFiles::from("./"))
+        .launch();
+
+    Ok(())
 }
