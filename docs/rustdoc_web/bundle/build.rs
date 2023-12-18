43c4eff198119431622e873e163771381d6fb4fa
NakagamiYuta <guguru0014@gmail.com>
2023-12-19 03:32:52 +0900

    [change] rustdoc_web の features の設定を変更

--- docs/rustdoc_web/bundle/build.rs
+++ docs/rustdoc_web/bundle/build.rs
@@ -5,28 +5,28 @@ fn main() {
     let current_dir = env::current_dir().unwrap();
     let css_path = current_dir.join("style.css");
 
-    #[cfg(feature = "embedded")]
-    Command::new("cargo")
-        .args(&["doc"])
-        .args(&["--lib"])
-        .args(&["--workspace"])
-        .args(&["--exclude", "ui"])
-        .args(&["--target-dir", "./docs/rustdoc_web/bundle/target"])
-        .args(&["--config", &format!("build.rustdocflags = [\"--extend-css\", \"{}\"]", css_path.display())])
-        .current_dir("../../../")
-        .status()
-        .unwrap();
-
-    #[cfg(not(feature = "embedded"))]
-    Command::new("cargo")
-        .args(&["doc"])
-        .args(&["--lib"])
-        .args(&["--workspace"])
-        .args(&["--exclude", "ui"])
-        .args(&["--target-dir", "./docs/rustdoc_web/bundle/target"])
-        .current_dir("../../../")
-        .status()
-        .unwrap();
+    if cfg!(feature = "embedded") {
+        Command::new("cargo")
+            .args(&["doc"])
+            .args(&["--lib"])
+            .args(&["--workspace"])
+            .args(&["--exclude", "ui"])
+            .args(&["--target-dir", "./docs/rustdoc_web/bundle/target"])
+            .args(&["--config", &format!("build.rustdocflags = [\"--extend-css\", \"{}\"]", css_path.display())])
+            .current_dir("../../../")
+            .status()
+            .unwrap();
+    } else {
+        Command::new("cargo")
+            .args(&["doc"])
+            .args(&["--lib"])
+            .args(&["--workspace"])
+            .args(&["--exclude", "ui"])
+            .args(&["--target-dir", "./docs/rustdoc_web/bundle/target"])
+            .current_dir("../../../")
+            .status()
+            .unwrap();
+    }
 
     println!("cargo:rerun-if-changed=style.css");
     println!("cargo:rerun-if-changed=../../../catalog");
