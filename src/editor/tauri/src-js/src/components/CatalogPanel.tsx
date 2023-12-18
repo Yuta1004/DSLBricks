a54d9ab2cbcdec99eb5ab74cc8b29aab39a8345a
NakagamiYuta <guguru0014@gmail.com>
2023-12-18 19:34:02 +0900

    [fix] 初期表示されている Catalog から別ページへジャンプできないバグを修正

--- src/editor/tauri/src-js/src/components/CatalogPanel.tsx
+++ src/editor/tauri/src-js/src/components/CatalogPanel.tsx
@@ -3,7 +3,7 @@ type CatalogPanelProps = {}
 export default function CatalogPanel(props: CatalogPanelProps) {
     return (
         <iframe
-            src="http://localhost:5555/catalog"
+            src="http://localhost:5555/catalog/index.html"
             style={{
                 width: "100%",
                 height: "calc(100% - 8px)",
