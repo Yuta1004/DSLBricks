9bd118ab2aab974bc1b45d9937af9b54586da148
NakagamiYuta <guguru0014@gmail.com>
2023-12-18 16:35:02 +0900

    [update] command::verify 内の Command に関する処理を Subprocess 構造体にまとめた

--- src/editor/tauri/src-rs/src/command/verify.rs
+++ src/editor/tauri/src-rs/src/command/verify.rs
@@ -2,95 +2,116 @@ use std::thread;
 use std::process::{Command, Child};
 use std::io::{Write, BufRead, BufReader};
 use std::time::Duration;
+use std::sync::{Arc, Mutex};
 
 use os_pipe::PipeWriter;
 use tauri::InvokeError;
 
-static mut PROCESS: Option<Child> = None;
-static mut PROCESS_STDIN: Option<PipeWriter> = None;
-static mut PROCESS_STDOUT: Option<Vec<String>> = None;
+static mut SUBPROCESS: Option<Arc<Mutex<Subprocess>>> = None;
 
 #[tauri::command]
 pub fn create_verify_process() -> Result<(), InvokeError> {
-    // Check PROCESS variable
     unsafe {
-        if PROCESS.is_some() {
+        if SUBPROCESS.is_some() {
             return Err(InvokeError::from("Verification process is already launched."));
         }
     }
 
-    // Launch a verification process
-    let (stdin_process, stdin_us) = os_pipe::pipe().unwrap();
-    let (stdout_us, stdout_process) = os_pipe::pipe().unwrap();
-    let process = Command::new("bash")
-        .stdin(stdin_process)
-        .stdout(stdout_process.try_clone().unwrap())
-        .stderr(stdout_process)
-        .spawn()
-        .unwrap();
-
-    // Start thread to capture stdout
-    thread::spawn(move || {
-        for line in BufReader::new(stdout_us).lines() {
-            if let Ok(line) = line {
-                unsafe { PROCESS_STDOUT.as_mut().unwrap().push(line); }
-            }
+    match Subprocess::new("bash") {
+        Ok(subprocess) => {
+            unsafe { SUBPROCESS = Some(subprocess); }
+            Ok(())
         }
-    });
-
-    // Update PROCESS variable
-    unsafe {
-        PROCESS = Some(process);
-        PROCESS_STDIN = Some(stdin_us);
-        PROCESS_STDOUT = Some(vec![]);
+        Err(err) => Err(InvokeError::from(format!("{}", err)))
     }
-
-    Ok(())
 }
 
 #[tauri::command]
 pub fn connect_verify_process(msg: &str) -> Result<String, InvokeError> {
-    // Attach to a verification process
-    let (_, stdin) = unsafe {
-        match &mut PROCESS {
-            Some(process) => (process, PROCESS_STDIN.as_mut().unwrap()),
+    let subprocess = unsafe {
+        match &SUBPROCESS {
+            Some(subprocess) => subprocess,
             None => return Err(InvokeError::from("Verification process is not launched."))
         }
     };
 
-    // Send msg to verification process
-    write!(stdin, "{}", msg).unwrap();
-    thread::sleep(Duration::from_secs(1));
+    Subprocess::stdin(Arc::clone(subprocess), msg).unwrap();
+    thread::sleep(Duration::from_millis(500));
+    let stdout = Subprocess::stdout(Arc::clone(subprocess)).unwrap();
 
-    // Receive msg from verification process
-    let recv = unsafe {
-        let recv = PROCESS_STDOUT.as_ref().unwrap().join("\n");
-        PROCESS_STDOUT = Some(vec![]);
-        recv
-    };
-
-    Ok(recv)
+    Ok(stdout)
 }
 
 #[tauri::command]
 pub fn finish_verify_process() -> Result<(), InvokeError> {
-    // Attach to a verification process
-    let (process, _) = unsafe {
-        match &mut PROCESS {
-            Some(process) => (process, PROCESS_STDIN.as_mut().unwrap()),
+    let subprocess = unsafe {
+        match &SUBPROCESS {
+            Some(subprocess) => subprocess,
             None => return Err(InvokeError::from("Verification process is not launched."))
         }
     };
 
-    // Kill the process
-    process.kill().unwrap();
-
-    // Reset variables
-    unsafe {
-        PROCESS = None;
-        PROCESS_STDIN = None;
-        PROCESS_STDOUT = None;
-    }
+    Subprocess::kill(Arc::clone(subprocess)).unwrap();
+    unsafe { SUBPROCESS = None; }
 
     Ok(())
+}
+
+struct Subprocess {
+    process: Child,
+    stdin: PipeWriter,
+    stdout: Vec<String>,
+}
+
+impl Subprocess {
+    pub fn new(cmd: &str) -> anyhow::Result<Arc<Mutex<Self>>> {
+        let (stdin_process, stdin_us) = os_pipe::pipe()?;
+        let (stdout_us, stdout_process) = os_pipe::pipe()?;
+        let process = Command::new(cmd)
+            .stdin(stdin_process)
+            .stdout(stdout_process.try_clone().unwrap())
+            .stderr(stdout_process)
+            .spawn()?;
+
+        // Init myself
+        let subprocess = Subprocess {
+            process,
+            stdin: stdin_us,
+            stdout: vec![],
+        };
+        let subprocess = Arc::new(Mutex::new(subprocess));
+
+        // Start thread to capture stdout
+        let subprcess_in_thread = Arc::clone(&subprocess);
+        thread::spawn(move || {
+            for line in BufReader::new(stdout_us).lines() {
+                if let Ok(line) = line {
+                    subprcess_in_thread
+                        .lock()
+                        .unwrap()
+                        .stdout
+                        .push(line);
+                }
+            }
+        });
+
+        Ok(subprocess)
+    }
+
+    pub fn stdin(subprocess: Arc<Mutex<Self>>, input: &str) -> anyhow::Result<()> {
+        let mut stdin = &subprocess.lock().unwrap().stdin;
+        write!(&mut stdin, "{}", input)?;
+        Ok(())
+    }
+
+    pub fn stdout(subprocess: Arc<Mutex<Self>>) -> anyhow::Result<String> {
+        let stdout = subprocess.lock().unwrap().stdout.join("\n");
+        *(&mut subprocess.lock().unwrap().stdout) = vec![];
+        Ok(stdout)
+    }
+
+    pub fn kill(subprocess: Arc<Mutex<Self>>) -> anyhow::Result<()> {
+        subprocess.lock().unwrap().process.kill()?;
+        Ok(())
+    }
 }
