b1398647b24ea2d38e36c864f1c55c21536be6b4
NakagamiYuta <guguru0014@gmail.com>
2023-12-18 22:49:19 +0900

    [fix] Editor 上での rustdoc の見た目を修正

--- docs/web/build.rs
+++ docs/web/build.rs
@@ -1,10 +1,14 @@
+use std::env;
 use std::process::Command;
 
 fn main() {
+    let current_dir = env::current_dir().unwrap();
+    let css_path = current_dir.join("style.css");
+
     Command::new("cargo")
         .args(&["doc"])
-        .args(&["-p", "catalog"])
-        .args(&["--exclude", "docs_web"])
+        .args(&["--lib"])
+        .args(&["--config", &format!("build.rustdocflags = [\"--extend-css\", \"{}\"]", css_path.display())])
         .current_dir("../../")
         .status()
         .unwrap();
