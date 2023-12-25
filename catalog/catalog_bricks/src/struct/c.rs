5f6ddf1cc2a013683aa1aea5bf5e4ce0a8184eba
NakagamiYuta <guguru0014@gmail.com>
2023-12-26 04:10:36 +0900

    [add] struct::c::Struct 作成

---
+++ catalog/catalog_bricks/src/struct/c.rs
@@ -0,0 +1,46 @@
+use std::cell::RefCell;
+
+use compiler::designer::design::macros::*;
+use compiler::designer::design::syntax::{Rule, RuleSet};
+use compiler::designer::design::DSLGeneratable;
+
+use macros::*;
+
+use crate::common::*;
+use crate::constraints::ctime::*;
+
+/// # 構造体
+///
+/// ## 概要
+///
+/// - C 言語の構造体定義を表現します
+///
+/// ## 性質
+/// - Identifiable
+/// - Calculatable
+#[derive(Default)]
+#[dslbrick(namespace = std.struct.c, property = DeclaredObject)]
+pub struct Struct {
+    #[component(single = Identifiable)]
+    id: RefCell<Option<Rule>>,
+}
+
+impl DSLBrickDesign for Struct {
+    fn design(&self) -> Vec<Rule> {
+        let mut rules = vec![
+            rule! { Struct -> "struct" id r"\{" members r"\}" },
+            rule! { members -> members member },
+            rule! { members -> member },
+            rule! { members -> },
+            rule! { member -> "int" id ";" },
+        ];
+        rules.extend(self.id.borrow().clone());
+        rules
+    }
+}
+
+impl DSLBrickAssertion for Struct {
+    fn assert(&self) {
+        assert!(self.id.borrow().is_some())
+    }
+}
--- examples/minic_interpreter/build.rs
+++ examples/minic_interpreter/build.rs
@@ -3,8 +3,8 @@ use catalog::primitive::number::integer::DecimalInteger;
 use catalog::primitive::number::fraction::DecimalFraction;
 use catalog::primitive::identifier::CStyleIdentifier;
 use catalog::statement::c::{Block, ExprStatement, If, For, While};
-use catalog::statement::StatementSet;
 use catalog::function::c::Function;
+use catalog::r#struct::c::Struct;
 use catalog::prelude::*;
 use catalog::macros::combine_bricks;
 
@@ -50,13 +50,13 @@ fn main() {
         stmt: [expr_stmt, if_stmt, for_stmt, while_stmt],
     };
 
-    // 文の集合
-    let stmts = StatementSet {
-        stmt: [block_stmt],
+    // 関数
+    let function = Function {
+        id: identifier,
+        stmt: block_stmt,
     };
 
-    Function {
+    Struct {
         id: identifier,
-        stmt: stmts,
     }
 }
