991adbdf89ad02785dc15a7c66fa77358545c1c5
NakagamiYuta <guguru0014@gmail.com>
2023-12-26 03:50:59 +0900

    [add] primitive::identifier::CStyleIdentifier 作成

---
+++ catalog/catalog_bricks/src/primitive/identifier.rs
@@ -0,0 +1,34 @@
+use compiler::designer::design::macros::*;
+use compiler::designer::design::syntax::{Rule, RuleSet};
+use compiler::designer::design::DSLGeneratable;
+
+use macros::*;
+
+use crate::common::*;
+use crate::constraints::ctime::*;
+
+/// # 識別子（C言語）
+///
+/// ## 概要
+///
+/// - C 言語の定義に従う識別子を表現します
+///
+/// ## 性質
+/// - Identifiable
+#[derive(Default)]
+#[dslbrick(namespace = std.primitive.identifier, property = Identifiable)]
+pub struct CStyleIdentifier;
+
+impl DSLBrickDesign for CStyleIdentifier {
+    fn design(&self) -> Vec<Rule> {
+        vec![
+            rule! { CStyleIdentifier -> "[_a-zA-Z][_a-zA-Z0-9]*" }
+        ]
+    }
+}
+
+impl DSLBrickAssertion for CStyleIdentifier {
+    fn assert(&self) {
+        // do nothing
+    }
+}
