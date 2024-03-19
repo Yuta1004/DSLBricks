9cc2d27038522e57f646f5b05d3dd4c2cdbeb9e3
NakagamiYuta <guguru0014@gmail.com>
2024-03-19 14:14:50 +0900

    [change] DSL 構造体の定義場所を変更

--- compiler/crates/core/processor/src/lib.rs
+++ compiler/crates/core/processor/src/lib.rs
@@ -1,4 +1,3 @@
-pub use dslgen::*;
 pub mod lexer {
     pub use lexer::*;
     pub use lexer_macros as macros;
@@ -6,4 +5,40 @@ pub mod lexer {
 pub mod parser {
     pub use parser::*;
     pub use parser_macros as macros;
+}
+
+#[cfg(feature = "with-serde")]
+use serde::{Deserialize, Serialize};
+
+use lexer::{LexIterator, Lexer, TokenSet};
+use parser::syntax::{post, pre};
+use parser::Parser;
+
+#[cfg_attr(feature = "with-serde", derive(Serialize, Deserialize))]
+pub struct DSL<PostS, PreS, T>(Lexer<T>, Parser<PostS, PreS, T>)
+where
+    PostS: post::Syntax<PreS, T>,
+    PreS: pre::Syntax<PostS, T>,
+    T: TokenSet;
+
+impl<PostS, PreS, T> DSL<PostS, PreS, T>
+where
+    PostS: post::Syntax<PreS, T>,
+    PreS: pre::Syntax<PostS, T> + 'static,
+    T: TokenSet + 'static,
+{
+    pub fn gen() -> anyhow::Result<DSL<PostS, PreS, T>> {
+        let lexer = Lexer::<T>::new()?;
+        let parser = Parser::<PostS, PreS, T>::new()?;
+        Ok(DSL(lexer, parser))
+    }
+
+    pub fn process<'a>(&self, input: &'a str) -> anyhow::Result<(Box<PostS>, Option<&'a str>)> {
+        let DSL(lexer, parser) = self;
+        let mut lexer = lexer.lex(input);
+        match parser.parse(&mut lexer) {
+            Ok(result) => Ok((result, lexer.remain())),
+            Err(err) => Err(err.into()),
+        }
+    }
 }
