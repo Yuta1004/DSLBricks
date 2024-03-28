fa27dbbc4b7c22476d40a34fdbbc9cb0a870eb49
NakagamiYuta <guguru0014@gmail.com>
2024-03-28 15:55:40 +0900

    [update] rustdoc_web 削除 & editor/srf-js に直接バンドルするようにした

--- src/editor/src-rs/build.rs
+++ src/editor/src-rs/build.rs
@@ -1,18 +1,17 @@
 use std::env;
+use std::path::Path;
 use std::process::Command;
 
 fn main() {
     // Catalog (lib)
     blockly::front::gen_ts_files("../src-js/src/custom", catalog_reimpl::catalog()).unwrap();
 
-    // Catalog (bin)
-    let current_dir = env::current_dir().unwrap();
-    Command::new("make")
-        .args(["bin"])
-        .args(&[format!("OUT_DIR={}", current_dir.display())])
-        .current_dir("../../../src/rustdoc_web")
-        .status()
-        .unwrap();
+    // Catalog (doc)
+    let cur_dir = env::current_dir().unwrap();
+    let doc_dir = Path::new(&cur_dir).join("../src-js/public/rustdoc");
+    if !doc_dir.exists() {
+        panic!("Bundle files not found. Please run `make setup` from the root directory first.");
+    }
 
     // React project
     Command::new("make")
@@ -24,14 +23,10 @@ fn main() {
     // Tauri
     tauri_build::build();
 
-    println!("cargo:rerun-if-changed=../../rustdoc_web/src/main.rs");
-    println!("cargo:rerun-if-changed=../../rustdoc_web/crates/rustdoc_bundle/src/lib.rs");
-    println!("cargo:rerun-if-changed=../../rustdoc_web/crates/rustdoc_bundle/build.rs");
-    println!("cargo:rerun-if-changed=../../rustdoc_web/crates/rustdoc_bundle/style.css");
-
     println!("cargo:rerun-if-changed=../src-js/src/components");
     println!("cargo:rerun-if-changed=../src-js/src/pages");
     println!("cargo:rerun-if-changed=../src-js/src/tauri");
+    println!("cargo:rerun-if-changed=../src-js/public");
     println!("cargo:rerun-if-changed=../src-js/next.config.js");
     println!("cargo:rerun-if-changed=../src-js/package.json");
     println!("cargo:rerun-if-changed=../src-js/tsconfig.json");
--- src/rustdoc_web/crates/rustdoc_bundle/src/lib.rs
+++
@@ -1,5 +0,0 @@
-use rust_embed::RustEmbed;
-
-#[derive(RustEmbed)]
-#[folder = "./rustdoc"]
-pub struct RustDoc;
